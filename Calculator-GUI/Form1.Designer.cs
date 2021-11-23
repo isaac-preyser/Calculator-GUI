0c281a3e73e3e2a0e336fd67750192933700bc44
1001155 <1001155@W63L018S03>
2021-11-23 12:14:08 -0800

    Add project files.

---
+++ Calculator-GUI/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace Calculator_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nmbr1 = new System.Windows.Forms.Button();
+            this.nmbr2 = new System.Windows.Forms.Button();
+            this.nmbr3 = new System.Windows.Forms.Button();
+            this.nmbr4 = new System.Windows.Forms.Button();
+            this.nmbr5 = new System.Windows.Forms.Button();
+            this.nmbr6 = new System.Windows.Forms.Button();
+            this.nmbr7 = new System.Windows.Forms.Button();
+            this.nmbr8 = new System.Windows.Forms.Button();
+            this.nmbr9 = new System.Windows.Forms.Button();
+            this.nmbr0 = new System.Windows.Forms.Button();
+            this.operEquals = new System.Windows.Forms.Button();
+            this.operAdd = new System.Windows.Forms.Button();
+            this.operSubtract = new System.Windows.Forms.Button();
+            this.operMult = new System.Windows.Forms.Button();
+            this.operDiv = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // nmbr1
+            // 
+            this.nmbr1.Location = new System.Drawing.Point(207, 96);
+            this.nmbr1.Name = "nmbr1";
+            this.nmbr1.Size = new System.Drawing.Size(37, 34);
+            this.nmbr1.TabIndex = 0;
+            this.nmbr1.Text = "1";
+            this.nmbr1.UseVisualStyleBackColor = true;
+            this.nmbr1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nmbr2
+            // 
+            this.nmbr2.Location = new System.Drawing.Point(263, 96);
+            this.nmbr2.Name = "nmbr2";
+            this.nmbr2.Size = new System.Drawing.Size(37, 34);
+            this.nmbr2.TabIndex = 1;
+            this.nmbr2.Text = "2";
+            this.nmbr2.UseVisualStyleBackColor = true;
+            this.nmbr2.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // nmbr3
+            // 
+            this.nmbr3.Location = new System.Drawing.Point(319, 96);
+            this.nmbr3.Name = "nmbr3";
+            this.nmbr3.Size = new System.Drawing.Size(37, 34);
+            this.nmbr3.TabIndex = 2;
+            this.nmbr3.Text = "3";
+            this.nmbr3.UseVisualStyleBackColor = true;
+            this.nmbr3.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // nmbr4
+            // 
+            this.nmbr4.Location = new System.Drawing.Point(207, 145);
+            this.nmbr4.Name = "nmbr4";
+            this.nmbr4.Size = new System.Drawing.Size(37, 34);
+            this.nmbr4.TabIndex = 3;
+            this.nmbr4.Text = "4";
+            this.nmbr4.UseVisualStyleBackColor = true;
+            this.nmbr4.Click += new System.EventHandler(this.nmbr4_Click);
+            // 
+            // nmbr5
+            // 
+            this.nmbr5.Location = new System.Drawing.Point(263, 145);
+            this.nmbr5.Name = "nmbr5";
+            this.nmbr5.Size = new System.Drawing.Size(37, 34);
+            this.nmbr5.TabIndex = 4;
+            this.nmbr5.Text = "5";
+            this.nmbr5.UseVisualStyleBackColor = true;
+            this.nmbr5.Click += new System.EventHandler(this.nmbr5_Click);
+            // 
+            // nmbr6
+            // 
+            this.nmbr6.Location = new System.Drawing.Point(319, 145);
+            this.nmbr6.Name = "nmbr6";
+            this.nmbr6.Size = new System.Drawing.Size(37, 34);
+            this.nmbr6.TabIndex = 5;
+            this.nmbr6.Text = "6";
+            this.nmbr6.UseVisualStyleBackColor = true;
+            this.nmbr6.Click += new System.EventHandler(this.nmbr6_Click);
+            // 
+            // nmbr7
+            // 
+            this.nmbr7.Location = new System.Drawing.Point(207, 195);
+            this.nmbr7.Name = "nmbr7";
+            this.nmbr7.Size = new System.Drawing.Size(37, 34);
+            this.nmbr7.TabIndex = 6;
+            this.nmbr7.Text = "7";
+            this.nmbr7.UseVisualStyleBackColor = true;
+            this.nmbr7.Click += new System.EventHandler(this.nmbr7_Click);
+            // 
+            // nmbr8
+            // 
+            this.nmbr8.Location = new System.Drawing.Point(263, 195);
+            this.nmbr8.Name = "nmbr8";
+            this.nmbr8.Size = new System.Drawing.Size(37, 34);
+            this.nmbr8.TabIndex = 7;
+            this.nmbr8.Text = "8";
+            this.nmbr8.UseVisualStyleBackColor = true;
+            this.nmbr8.Click += new System.EventHandler(this.nmbr8_Click);
+            // 
+            // nmbr9
+            // 
+            this.nmbr9.Location = new System.Drawing.Point(319, 195);
+            this.nmbr9.Name = "nmbr9";
+            this.nmbr9.Size = new System.Drawing.Size(37, 34);
+            this.nmbr9.TabIndex = 8;
+            this.nmbr9.Text = "9";
+            this.nmbr9.UseVisualStyleBackColor = true;
+            this.nmbr9.Click += new System.EventHandler(this.nmbr9_Click);
+            // 
+            // nmbr0
+            // 
+            this.nmbr0.Location = new System.Drawing.Point(207, 235);
+            this.nmbr0.Name = "nmbr0";
+            this.nmbr0.Size = new System.Drawing.Size(37, 34);
+            this.nmbr0.TabIndex = 9;
+            this.nmbr0.Text = "0";
+            this.nmbr0.UseVisualStyleBackColor = true;
+            this.nmbr0.Click += new System.EventHandler(this.nmbr0_Click);
+            // 
+            // operEquals
+            // 
+            this.operEquals.Location = new System.Drawing.Point(263, 235);
+            this.operEquals.Name = "operEquals";
+            this.operEquals.Size = new System.Drawing.Size(93, 34);
+            this.operEquals.TabIndex = 10;
+            this.operEquals.Text = "=";
+            this.operEquals.UseVisualStyleBackColor = true;
+            this.operEquals.Click += new System.EventHandler(this.operEquals_Click);
+            // 
+            // operAdd
+            // 
+            this.operAdd.Location = new System.Drawing.Point(371, 96);
+            this.operAdd.Name = "operAdd";
+            this.operAdd.Size = new System.Drawing.Size(37, 34);
+            this.operAdd.TabIndex = 11;
+            this.operAdd.Text = "+";
+            this.operAdd.UseVisualStyleBackColor = true;
+            this.operAdd.Click += new System.EventHandler(this.operAdd_Click);
+            // 
+            // operSubtract
+            // 
+            this.operSubtract.Location = new System.Drawing.Point(371, 145);
+            this.operSubtract.Name = "operSubtract";
+            this.operSubtract.Size = new System.Drawing.Size(37, 34);
+            this.operSubtract.TabIndex = 12;
+            this.operSubtract.Text = "-";
+            this.operSubtract.UseVisualStyleBackColor = true;
+            this.operSubtract.Click += new System.EventHandler(this.operSubtract_Click);
+            // 
+            // operMult
+            // 
+            this.operMult.Location = new System.Drawing.Point(371, 195);
+            this.operMult.Name = "operMult";
+            this.operMult.Size = new System.Drawing.Size(37, 34);
+            this.operMult.TabIndex = 13;
+            this.operMult.Text = "x";
+            this.operMult.UseVisualStyleBackColor = true;
+            this.operMult.Click += new System.EventHandler(this.operMult_Click);
+            // 
+            // operDiv
+            // 
+            this.operDiv.Location = new System.Drawing.Point(371, 235);
+            this.operDiv.Name = "operDiv";
+            this.operDiv.Size = new System.Drawing.Size(37, 34);
+            this.operDiv.TabIndex = 14;
+            this.operDiv.Text = "/";
+            this.operDiv.UseVisualStyleBackColor = true;
+            this.operDiv.Click += new System.EventHandler(this.operDiv_Click);
+            // 
+            // output
+            // 
+            this.output.AutoSize = true;
+            this.output.BackColor = System.Drawing.SystemColors.Control;
+            this.output.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.output.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.output.Location = new System.Drawing.Point(205, 56);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(151, 27);
+            this.output.TabIndex = 15;
+            this.output.Text = "placeholder text";
+            this.output.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.operDiv);
+            this.Controls.Add(this.operMult);
+            this.Controls.Add(this.operSubtract);
+            this.Controls.Add(this.operAdd);
+            this.Controls.Add(this.operEquals);
+            this.Controls.Add(this.nmbr0);
+            this.Controls.Add(this.nmbr9);
+            this.Controls.Add(this.nmbr8);
+            this.Controls.Add(this.nmbr7);
+            this.Controls.Add(this.nmbr6);
+            this.Controls.Add(this.nmbr5);
+            this.Controls.Add(this.nmbr4);
+            this.Controls.Add(this.nmbr3);
+            this.Controls.Add(this.nmbr2);
+            this.Controls.Add(this.nmbr1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button nmbr1;
+        private System.Windows.Forms.Button nmbr2;
+        private System.Windows.Forms.Button nmbr3;
+        private System.Windows.Forms.Button nmbr4;
+        private System.Windows.Forms.Button nmbr5;
+        private System.Windows.Forms.Button nmbr6;
+        private System.Windows.Forms.Button nmbr7;
+        private System.Windows.Forms.Button nmbr8;
+        private System.Windows.Forms.Button nmbr9;
+        private System.Windows.Forms.Button nmbr0;
+        private System.Windows.Forms.Button operEquals;
+        private System.Windows.Forms.Button operAdd;
+        private System.Windows.Forms.Button operSubtract;
+        private System.Windows.Forms.Button operMult;
+        private System.Windows.Forms.Button operDiv;
+        private System.Windows.Forms.Label output;
+    }
+}
+
---
+++ Calculator-GUI/Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Calculator_GUI
+{
+    public partial class Form1 : Form
+    {
+        //lists that save numbers for the calculator. the first one saves the inputted numbers, the second saves the saved numbers after an operation has been selected. 
+        List<int> inputtedNumber = new List<int>(); //this list takes the numbers from the numpad that the user inputs and saves them.
+
+        //TODO: Make inputtedNumber use int32, and then output label converts to string.
+
+        List<int> savedNumbers = new List<int>();   //this list contains formatted numbers the computer can use in calculations. these numbers are derived from the inputtedNumber by essentially concatenating all the numbers. 
+        List<char> pendingOperations = new List<char>();    //this list contains the operations that need to be conducted. The idea behind this is that item 0 of saved numbers will have a corresponding operation at item 0 of this list. Upon clicking equals, the computer will load the first operation, i.e. add, and then load the first and second numbers. it will then perform that operation. then, if there is another operation, it will load the output of the previous operation, and perform it with the next saved number. this chain will repeat until there are no more operations to carry out. 
+
+        //should look like: 
+        /*
+         * Read: pendingOps[i] .... == '+'
+         * Perform Math.Add on savedNumbers[i] and savedNumbers[i + 1] 
+         * Output: = sum of savedNumbers i and i+1, save to output variable
+         * i++
+         * Read: next pendingOps ... == '-'
+         * Perform Math.Subtract on output and savedNumbers[i+1]
+         * Output: some output, save to output variable
+         * i++
+         * Read next pendingOps ... == '='
+         * Perform no task- display current output to label. 
+         * loop ends- '=' is always last in the op list. 
+         * 
+         */
+
+
+        public List<int> Cache(int input)
+        {
+
+
+            //create list
+            
+            inputtedNumber.Add(input);
+
+            //add input number to the label
+
+            output.Text += input;
+            output.Refresh();
+
+
+            //output list to be used in other methods. 
+
+            return inputtedNumber;
+            
+
+        }
+
+        public void Calculate(char oper)
+        {
+            //this method is to be run if the cache method detects an operator; this method will detect which operator it is, and point it to the correct operation. This method will also store the inputted number as one, to be properly manipulated by the System.Math class. 
+
+            //as of right now, this method is broken.
+
+            int workingNumber = 0;
+
+            string concatList = ""; //this is to smush all the numbers together into one string, to then be parsed into a proper number. 
+
+
+
+            if (oper == '=')
+            {
+                foreach (var value in inputtedNumber)
+                {
+                    concatList += Convert.ToString(value);
+
+                }
+
+                int numToAdd = Int32.Parse(concatList);
+                savedNumbers.Add(numToAdd); //saves formatted number from inputtedNumber and saves it in SavedNumbers for calculation later.
+                pendingOperations.Add('='); //saves operation to the op list for calculation later, in this case the final operation.
+                inputtedNumber.Clear(); 
+
+                foreach (var value in pendingOperations)
+                {
+                    int a = pendingOperations.IndexOf(value); //finds the index of the current operation, to load in the correctly corresponding number value in savedNumbers.
+                    int b = a + 1;
+
+                    switch (value)
+                    {
+                        case '+':
+                            workingNumber = savedNumbers[a] + savedNumbers[b];
+                            savedNumbers.Insert(b, workingNumber); //insert now worked on number in the location of b, which will be a on the next time through. This allows for operations such as 4 + 5 + 6, by going 4 + 5 = 9, and then 9 + 6 = 15.
+                            break;
+                        case '-':
+                            workingNumber = savedNumbers[a] - savedNumbers[b];
+                            savedNumbers.Insert(b, workingNumber);
+                            break;
+                        case 'x':
+                            workingNumber = savedNumbers[a] * savedNumbers[b];
+                            savedNumbers.Insert(b, workingNumber);
+                            break;
+                        case '/':
+                            workingNumber = savedNumbers[a] / savedNumbers[b];
+                            savedNumbers.Insert(b, workingNumber);
+                            break;
+                        case '=': 
+                            output.Text = Convert.ToString(workingNumber);
+                            savedNumbers.Clear();
+                            savedNumbers.Add(workingNumber);    //clears savedNumbers, and saves the output as the first number, so one could go 1+1 = 2, and then go +3 = and recieve 5. TODO: add a CLEAR button.
+                            output.Refresh();
+                            return;
+
+
+
+                    }
+
+
+
+
+
+                }
+                return;
+            }
+
+            if (oper == '+')
+            {
+                foreach (var value in inputtedNumber)
+                {
+                    concatList += Convert.ToString(value);
+
+                }
+
+                int numToAdd = Int32.Parse(concatList);
+                savedNumbers.Add(numToAdd); //saves formatted number from inputtedNumber and saves it in SavedNumbers for calculation later.
+                pendingOperations.Add('+'); //saves operation to the op list for calculation later.
+                inputtedNumber.Clear();
+                return;
+
+            }
+
+
+            if (oper == '-')
+            {
+                return;
+            }
+
+
+            if (oper == 'x')
+            {
+                return;
+            }
+
+            if (oper == '/')
+            {
+                return; 
+            } else
+            {
+                throw new Exception("the inputted operation was has not been coded yet, or the operation is not possible");
+            }
+        }
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            output.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Local vars.. 
+            int number = 1;
+
+
+            //this is button one. winforms messed up and named this the wrong thing. 
+
+            Cache(number);
+
+            
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //local vars
+            int number = 2;
+
+            //this is button 2. same issue as above.
+            Cache(number);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int number = 3;
+
+            //button 3
+            Cache(number);
+        }
+
+        private void nmbr4_Click(object sender, EventArgs e)
+        {
+
+            int number = 4;
+
+            //button 4
+            Cache(number);
+        }
+
+        private void nmbr5_Click(object sender, EventArgs e)
+        {
+
+            int number = 5;
+
+            //button 5
+            Cache(number);
+        }
+
+        private void nmbr6_Click(object sender, EventArgs e)
+        {
+            //local vars
+            int number = 6;
+
+            Cache(number);
+        }
+
+        private void nmbr7_Click(object sender, EventArgs e)
+        {
+            //local vars
+            int number = 7;
+
+            Cache(number);
+        }
+
+        private void nmbr8_Click(object sender, EventArgs e)
+        {
+            //local vars
+            int number = 8;
+
+            Cache(number);
+        }
+
+        private void nmbr9_Click(object sender, EventArgs e)
+        {
+            //local vars 
+            int number = 9;
+
+            Cache(number);
+        }
+
+        private void nmbr0_Click(object sender, EventArgs e)
+        {
+            //local vars 
+            int number = 0;
+
+            Cache(number);
+        }
+
+        private void operEquals_Click(object sender, EventArgs e)
+        {
+            //local vars
+            char op = '=';
+
+            output.Text = $"{op}";
+            output.Refresh();
+            Calculate(op);
+            
+
+
+        }
+
+        private void operAdd_Click(object sender, EventArgs e)
+        {
+            //local vars
+            char op = '+';
+
+            output.Text = $"{op}";
+            output.Refresh();
+            Calculate(op);
+        }
+
+        private void operSubtract_Click(object sender, EventArgs e)
+        {
+            //local vars
+            char op = '-';
+
+            output.Text = $"{op}";
+            output.Refresh();
+        }
+
+        private void operMult_Click(object sender, EventArgs e)
+        {
+            //local vars
+            char op = 'x';
+            
+            output.Text = $"{op}";
+            output.Refresh();
+        }
+        private void operDiv_Click(object sender, EventArgs e)
+        {
+            //local vars
+            char op = '/';
+            
+            
+            output.Text = $"{op}";
+            output.Refresh();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //do nothing on label click, this should only be used for outputting numbers. 
+            return;
+        }
+    }
+}
